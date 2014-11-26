9664d01d1d2c391b894f29540d57ea8bfd7ba9f4
rumagishi <rumagishi@gmail.com>
2014-11-27 01:04:32 +0900

    Note viewers diagram

---
+++ login_interface/login_interface/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  login_interface
+//
+//  Created by Kaito Oshiro on 2014/11/26.
+//  Copyright (c) 2014年 team-g. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
