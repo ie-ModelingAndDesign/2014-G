2e17ee3f19e9606126692bd5aa4135616c1a9aab
tomonegi <strikewitches504@gmail.com>
2015-02-21 17:19:56 +0900

    kurukuru

--- Note_Viewer/User_Access/User_Access/ThirdViewController.swift
+++ Note_Viewer/User_Access/User_Access/ThirdViewController.swift
@@ -15,7 +15,13 @@ import Foundation
 
 import UIKit
 
-class ThirdViewController: UIViewController {
+class ThirdViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource  {
+    
+    // UIPickerView.
+    var myUIPicker: UIPickerView = UIPickerView()
+    
+    // 表示する値の配列.
+    var myValues: NSArray = ["その一","その二","その三","その四"]
     
     var myButtonBefore: UIButton!
     var myButtonToNote: UIButton!
@@ -30,18 +36,34 @@ class ThirdViewController: UIViewController {
         myImageView.frame = CGRectMake(-10, -28, myImage.size.width, myImage.size.height)
         self.view.addSubview(myImageView)
         
+        //-------------------------
+        // サイズを指定する.
+        myUIPicker.frame = CGRectMake(0,self.view.bounds.height/2,self.view.bounds.width, 180.0)
+        myUIPicker.backgroundColor = UIColor.grayColor()
+        // Delegateを設定する.
+        myUIPicker.delegate = self
+        
+        // DataSourceを設定する.
+        myUIPicker.dataSource = self
+        
+        // Viewに追加する.
+        self.view.addSubview(myUIPicker)
+        
+        //-------------------
+
+        
         // 背景色を設定.
         //self.view.backgroundColor = UIColor.blueColor()
         self.view.backgroundColor = UIColor.hexStr("79c0fe", alpha: 1)
         
         // ボタンの生成する.
-        myButtonToNote = UIButton(frame: CGRectMake(0,0,120,50))
+      /*  myButtonToNote = UIButton(frame: CGRectMake(0,0,120,50))
         myButtonToNote.backgroundColor = UIColor.redColor();
         myButtonToNote.layer.masksToBounds = true
         myButtonToNote.setTitle("6th Lecture", forState: .Normal)
         myButtonToNote.layer.cornerRadius = 20.0
         myButtonToNote.layer.position = CGPoint(x: self.view.bounds.width/2 , y:self.view.bounds.height-150)
-        myButtonToNote.addTarget(self, action: "onClickMyButtonToNext:", forControlEvents: .TouchUpInside)
+        myButtonToNote.addTarget(self, action: "onClickMyButtonToNext:", forControlEvents: .TouchUpInside)*/
         
         // UIボタンを作成
         myButtonBefore = UIButton(frame: CGRectMake(0,0,120,50))
@@ -54,7 +76,7 @@ class ThirdViewController: UIViewController {
         self.view.addSubview(myButtonBefore);
         
         // ボタンを追加する.
-        self.view.addSubview(myButtonToNote);
+        //self.view.addSubview(myButtonToNote);
         
     }
     //
@@ -92,7 +114,58 @@ class ThirdViewController: UIViewController {
         self.presentViewController(myViewController, animated: true, completion: nil)
     }
 
+    //---------------
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
     
+    /*
+    表示するデータ数を返す.
+    */
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return myValues.count
+    }
+    
+    /*
+    値を代入する.
+    */
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String {
+        return myValues[row] as String
+    }
+    
+    /*
+    Pickerが選択された際に呼ばれる.
+    */
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        //println("row: \(row)")
+        //println("value: \(myValues[row])")
+        
+        if row == 0{
+            // 遷移するViewを定義.
+            let myViewController: UIViewController = SecondViewController()
+            
+            // アニメーションを設定.
+            myViewController.modalTransitionStyle = UIModalTransitionStyle.FlipHorizontal
+            
+            // Viewの移動.
+            self.presentViewController(myViewController, animated: true, completion: nil)
+
+        }else {
+        // 遷移するViewを定義.
+        // ここでURLに飛ぶようにしたい
+        let noteurl = "http://ie.u-ryukyu.ac.jp/~e135761/PostingNote/information/information6.pdf"
+        //let myViewController: UIViewController = PDFViewController(noteurl)
+        let myViewController = PDFViewController()
+        myViewController.noteurl = noteurl
+        
+        // アニメーションを設定.
+        myViewController.modalTransitionStyle = UIModalTransitionStyle.FlipHorizontal
+        
+        // Viewの移動.
+        self.presentViewController(myViewController, animated: true, completion: nil)
+        //self.presentViewController(Filereader(), animated: true, completion: nil)
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
